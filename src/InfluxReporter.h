f4c35332564998ca3daec6b575ef9b5ad83569e3
Korobkov Dmitry <dmitry.korobkov.nn@gmail.com>
2026-02-15 19:42:43 +0300

    feat: metrics reporting library

---
+++ src/InfluxReporter.h
@@ -0,0 +1,28 @@
+/*
+ * Metrics reporting library
+ *
+ * Copyright (C) 2026 Dmitry Korobkov <dmitry.korobkov.nn@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+#include <string>
+
+#include <Client.h>
+#include <Reporter.h>
+
+namespace app {
+
+class InfluxReporter : public Reporter {
+public:
+    using Reporter::Reporter;
+
+    size_t send(uint32_t timestamp = 0) override;
+};
+
+} // namespace app
