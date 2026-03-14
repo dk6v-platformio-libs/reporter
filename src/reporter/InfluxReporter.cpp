6d03a29edf55abf57574bed2cec4814413dd4dc0
Korobkov Dmitry <dmitry.korobkov.nn@gmail.com>
2026-03-14 12:23:33 +0300

    fix: use local buffer

--- src/reporter/InfluxReporter.cpp
+++ src/reporter/InfluxReporter.cpp
@@ -7,55 +7,77 @@
  * (at your option) any later version.
  */
 
+#undef LOG_MODULE
+#define LOG_MODULE "REPORT"
+
 #include <Arduino.h>
 #include <Client.h>
+#include <console.h>
 
 #include "InfluxReporter.h"
 
 using namespace reporter;
 
-size_t InfluxReporter::send(uint32_t timestamp) {
-
-    std::string message = mMeasurement;
-
-    if (mMeasurement.empty() || (mHeaders.empty() && mTags.empty() && mFields.empty())) {
+size_t InfluxReporter::send(uint32_t timestamp)
+{
+    if (mMeasurement.empty() || (mHeaders.empty() && mTags.empty() && mFields.empty()))
         return 0;
-    }
 
-    if (!mClient_p->connect(mHost.c_str(), mPort)) {
+    if (!mClient_p->connect(mHost.c_str(), mPort))
         return 0;
+
+    char metric[160] = {0};
+    char *npos = metric;
+
+    auto append = [&](auto &&...args) -> bool
+    {
+        if (static_cast<size_t>(npos - metric) >= sizeof(metric))
+        {
+            LOGE("failed to send metric, out of metric");
+            return false;
+        }
+        npos += snprintf(npos, (sizeof(metric) - (npos - metric)),
+                             std::forward<decltype(args)>(args)...);
+        return true;
+    };
+
+    if (!append(mMeasurement.c_str()))
+        return 0;
+
+    for (auto it = mHeaders.begin(); it != mHeaders.end(); ++it)
+    {
+        if (!append(",%s=%s", std::get<NAME>(*it).c_str(), std::get<VALUE>(*it).c_str()))
+            return 0;
     }
 
-    // Add header and tags
-    for (const auto &name : mHeaders) {
-        message += ',' + std::get<NAME>(name) + '=' + std::get<VALUE>(name);
+    for (auto it = mTags.begin(); it != mTags.end(); ++it)
+    {
+        if (!append(",%s=%s", std::get<NAME>(*it).c_str(), std::get<VALUE>(*it).c_str()))
+            return 0;
     }
 
-    for (const auto &name : mTags) {
-        message += ',' + std::get<NAME>(name) + '=' + std::get<VALUE>(name);
+    for (auto it = mFields.begin(); it != mFields.end(); ++it)
+    {
+        if (!append("%c", (it == mFields.begin()) ? ' ' : ','))
+            return 0;
+        if (!append("%s=%s", std::get<NAME>(*it).c_str(), std::get<VALUE>(*it).c_str()))
+            return 0;
     }
 
-    // Add measurement fields
-    for (auto it = mFields.begin(); it != mFields.end(); ++it) {
-        message += (it == mFields.begin()) ? ' ' : ',';
-        message += std::get<NAME>(*it) + '=' + std::get<VALUE>(*it);
+    if ((timestamp != 0) && (timestamp != static_cast<uint32_t>(-1)))
+    {
+        if (!append(" %s000000000", std::to_string(timestamp).c_str()))
+            return 0;
     }
 
-    // Add timestamp if provided
-    if (timestamp != 0 && timestamp != static_cast<uint32_t>(-1)) {
-        message += ' ';
-        message += std::to_string(timestamp) + "000000000"; // Convert to nanoseconds
-    }
+    const size_t length = mClient_p->write(
+        reinterpret_cast<const uint8_t *>(metric),
+        strlen(metric));
 
-    message += '\n';
-
-    const size_t size = mClient_p->write(
-        reinterpret_cast<const uint8_t*>(message.c_str()),
-        message.length()
-    );
+    LOGI(metric);
 
     mClient_p->flush();
     mClient_p->stop();
 
-    return size;
+    return length;
 }
